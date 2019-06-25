78638a92a834c3b5c95d3962e6fd4e0d159ee0ce
Frederic Rezeau <frederic.rezeau@gmail.com>
2019-06-25 20:12:23 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+﻿# Litemint Official Website
+
+## License
+
+The source code is released under the [MIT License](https://github.com/litemint/litemint-web/blob/master/LICENSE).
+
+Copyright (c) 2019 Frederic (경진) Rezeau<br />
+Copyright (c) 2019 Litemint LLC
+
+> LITEMINT LLC owns all Litemint-related trademarks, service marks, and graphic logos and the names of all Litemint projects are trademarks of LITEMINT LLC.
+
+## Dependency Licenses
+
+### Bulma (Modern CSS framework based on Flexbox)
+
+<table>
+  <tr>
+    <td>URL</td>
+    <td>https://github.com/jgthms/bulma</td>
+  </tr>
+  <tr>
+    <td>License</td>
+    <td>MIT License</td>
+  </tr>
+  <tr>
+    <td>Local Modifications</td>
+    <td>CSS theme/color customizations</td>
+  </tr>
+</table>
---
+++ index.js
@@ -0,0 +1,64 @@
+﻿///////////////////////////////////////////////////////////////////////////////
+// file index.js
+// Copyright (c) 2018 Frédéric J. Rézeau. All rights reserved.
+///////////////////////////////////////////////////////////////////////////////
+
+$(document).ready(function () {
+    "use strict";
+
+    function numberWithCommas(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+    // Smooth scrolling.
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
+    $("#wallet-count").animate(
+        { value: parseInt($("#wallet-count").html()) },
+        {
+            duration: 1000,
+            step: function (now) { $(this).html(numberWithCommas(parseInt(now))); }
+        });
+
+    // Burger menu.
+    (function () {
+        var burger = document.querySelector('.navbar-burger');
+        var menu = document.querySelector('#' + burger.dataset.target);
+        burger.addEventListener('click', function () {
+            burger.classList.toggle('is-active');
+            menu.classList.toggle('is-active');
+        });
+    })();
+    // Top menu styling.
+    $(window).scroll(function () {
+        var pos = $(window).scrollTop();
+        if (pos === 0) {
+            $("#navbar").removeClass('site-navbar-scrolled');
+        }
+        else {
+            $("#navbar").addClass('site-navbar-scrolled');
+        }
+    });
+
+    $(".close-modal").click(function () {
+        $("#modalbox").removeClass("is-active");
+    });
+
+    $("#app-launcher").click(function () {
+        window.location = "https://litemint.app";
+    });
+
+    $.ajax("https://api.litemint.com/.market/getrates").then(
+        function success(response) {
+            var fee = "$" + (1 / response["XLM"].rate * 0.00001).toFixed(7); // 100 stroop
+            $("#fee").html(fee);
+        }
+    );
+});
